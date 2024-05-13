fb9319380a3b1eddf9c233268958014d199e0139
Elizabeth <itsalaz@icloud.com>
2024-05-13 16:31:17 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your code here!
+const main = document.getElementById("main")
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Elizabeth is the champion";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const main = document.getElementById("main");
+
+// main.remove();
+
+// const newHeader = document.createElement("h1");
+
+// newHeader.setAttribute("id","victory");
+
+// newHeader.textContent = "Elizabeth is the champion"
